08411d9f9fe651b0e5b7e2d5450cd933dadb13dd
David69-code <ba1155032024@unab.edu.sv>
2026-03-16 16:13:27 -0600

    Caso 1: Implementación de Patrón Builder para registro de Usuarios

---
+++ BuilderUsuario/LaboratorioSemana9/Caso 1/BuilderUsuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioSemana9.Caso_1
+{
+    public class UsuarioBuilder
+    {
+        private Usuario _usuario = new Usuario();
+
+        // Método para datos obligatorios al inicio
+        public UsuarioBuilder ConDatosBasicos(string nombre, string correo, string pass)
+        {
+            _usuario.Nombre = nombre;
+            _usuario.Correo = correo;
+            _usuario.Password = pass;
+            return this; // Retorna la misma instancia para encadenar métodos
+        }
+
+        public UsuarioBuilder ConRol(string rol)
+        {
+            _usuario.Rol = rol;
+            return this;
+        }
+
+        public UsuarioBuilder ConDireccion(string direccion)
+        {
+            _usuario.Direccion = direccion;
+            return this;
+        }
+
+        // Construye y retorna el objeto final
+        public Usuario Build() => _usuario;
+    }
+}
---
+++ BuilderUsuario/LaboratorioSemana9/Caso 1/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratorioSemana9.Caso_1
+{
+    public class Usuario
+    {
+        // Propiedades del perfil de usuario
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public bool Estado { get; set; }
+
+        public void MostrarPerfil()
+        {
+            System.Console.WriteLine($"--- Perfil de {Nombre} ---");
+            System.Console.WriteLine($"Rol: {Rol} | Email: {Correo}");
+            System.Console.WriteLine($"Dirección: {Direccion ?? "No proporcionada"}");
+            System.Console.WriteLine("---------------------------\n");
+        }
+    }
+}
